387af1a3586a0d98e0484758b58ba68e641cd26a
Andrea Witt <andreawitt14@hotmail.com>
2018-05-28 20:17:23 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,18 @@
+function theBeatlesPlay(musicians, instruments){
+  var newArray=[];
+  
+  for(let i = 0; i < musicians.length; i++){
+    newArray[i] = musicians[i].concat(" plays ", instruments[i])
+  }
+  
+  return newArray;
+  
+}
+
+function johnLennonFacts(facts){
+  var j = 0;
+  while(j <= facts.length()){
+    
+    
+  }
+}
